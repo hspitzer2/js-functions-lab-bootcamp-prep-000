2d0f677e82e80fa91dbf8678d80c07a0a3e2783d
Hillary Spitzer <hspitzer2@gmail.com>
2019-04-19 18:42:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// write your code below!
+var index = require("../index.js")
+var string = "Happy holidays!"
+
+function happyHolidays(string) {
+  console.log(string)
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!";
+}
+
+function sayGreeting(name) {
+  console.log("Happy holidays, you!");
+} 
+
+function sayString(holiday, name) {
+  console.log(`Happy ${holiday}, ${name}!`);{
+    return "Happy Independence Day, you!";
+  }
+}
